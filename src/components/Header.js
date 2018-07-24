ec4a32109eb6c1c5cdead897abec7aa2b887f0d5
Aivirth <aivirth.work@gmail.com>
2018-07-24 23:55:14 +0200

    Added Header functional component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// export default () => {
+//   return (
+//     <div>
+//       <h1>Contact Manager</h1>
+//     </div>
+//   );
+// };
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Contact Manager</h1>
+    </div>
+  );
+};
+
+export default Header;
