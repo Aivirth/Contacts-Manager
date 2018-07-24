8affb32fceebe52ae11891fda955f372ee7026f7
Aivirth <aivirth.work@gmail.com>
2018-07-24 23:49:03 +0200

    Added Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>john doe</h4>
+        <ul>
+          <li>Email: jdoe@example.com</li>
+          <li>Phone: 555-555-5555</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
