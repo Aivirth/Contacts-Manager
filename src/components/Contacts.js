88dd7dfed17945371b3af691cbbb76d7a9a6fcc8
Aivirth <aivirth.work@gmail.com>
2018-07-25 12:02:12 +0200

    added state to Contacts component

---
+++ src/components/Contacts.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+export default class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "john doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "karen doe",
+        email: "karen@gmail.com",
+        phone: "555-555-5455"
+      },
+      {
+        id: 3,
+        name: "jack doe",
+        email: "jack@gmail.com",
+        phone: "555-555-5559"
+      }
+    ]
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact
+            name={contact.name}
+            email={contact.email}
+            phone={contact.phone}
+            key={contact.id}
+          />
+        ))}
+      </div>
+    );
+  }
+}
