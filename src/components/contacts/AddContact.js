f5a55cde0cac63a4b6b4a9dcf14e9a49fcfa0529
Aivirth <aivirth.work@gmail.com>
2018-07-26 11:18:31 +0200

    Added basic form structure

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+export default class AddContact extends Component {
+  state = {
+    name: "",
+    email: "",
+    phone: ""
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="name"
+                placeholder="Enter Name..."
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                name="email"
+                placeholder="example@gmail.com"
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="phone"
+                placeholder="555-555-5555"
+                value={phone}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-block btn-light"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
